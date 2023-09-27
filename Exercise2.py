c7a0e4e816812fc215d931117eb30fcbe680b5e6
leojosue <leojosue2205@gmail.com>
2023-09-26 23:03:43 -0600

    Complete exercise two

--- Exercise2.py
+++ Exercise2.py
@@ -1,20 +1,33 @@
 with open("story.txt", "r") as f:
     story = f.read()
-    print(enumerate(story))
-    
-words = []
+
+#Declare a set because we need unique values 
+words = set()
 start_of_word = -1
 
 tarjet_start = "<"
 tarjet_end = ">"
 
 for i, char in enumerate(story):
+    
+    #Look for the character "<" and save his index and active flag
     if char == tarjet_start:
         start_of_word = i
-    
+    #Look for the character ">" and save his index return flag to initial value (-1)
     if char == tarjet_end and start_of_word != -1:
         word = story[start_of_word: i + 1]
-        words.append(word)
+        words.add(word)
         start_of_word = -1
 
-print(words)
+answers = {}
+
+#Loop the set words and save the inputs in a list
+for word in words :
+    answer = input("Enter a word for " + word + ": ")
+    answers[word] = answer
+
+#rewrite the story variable with the new string 
+for word in words:
+    story = story.replace(word,answers[word])
+    
+print(story)
